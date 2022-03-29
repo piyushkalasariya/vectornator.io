ed73412de4d01e55bda60c20bfaba6571e00583c
Varun Miyani <varunbhai.miyani@namastecredit.com>
2022-03-29 21:10:26 +0530

    div to a

--- algolia-custom.js
+++ algolia-custom.js
@@ -148,7 +148,7 @@ const getSearchList = (data) => {
           const HEADING = getHeading({ hit });
           const SEARCH_LINK = `${window.location.origin}/${hit.objectID}`;
           return `
-            <div href="${SEARCH_LINK}" class="st-link ${colorClass} w-inline-block" ${bindEvent(
+            <a href="${SEARCH_LINK}" class="st-link ${colorClass} w-inline-block" ${bindEvent(
             "click",
             hit,
             "Search Result Clicked"
@@ -161,7 +161,7 @@ const getSearchList = (data) => {
                   highlightedTagName: "strong",
                 })}
               </div>
-            </div>
+            </a>
           `;
         })
         .join("")}
